8182efdc21900840a4d5bc71a2532b49ec693093
Rebecca Samuel <se.rebecca.samuel@gmail.com>
2021-02-04 01:54:37 +0300

    made changes

--- TaskManager/assets/js/app.js
+++ TaskManager/assets/js/app.js
@@ -1,116 +1,170 @@
-$(document).ready(function() {
-    $(".dropdown-trigger").dropdown();
-});
+// Define UI Variables
+const taskInput = document.querySelector("#task"); //the task input text field
 
-// Define UI Variables 
+const form = document.querySelector("#task-form"); //The form at the top
 
-const taskInput = document.querySelector('#task');               //the task input text field
+const filter = document.querySelector("#filter"); //the task filter text field
 
-const form = document.querySelector('#task-form');             //The form at the top
+const taskList = document.querySelector(".collection"); //The ul
 
-const filter = document.querySelector('#filter');                      //the task filter text field
+const taskList2 = document.querySelector(".collection-sorted");
 
-const taskList = document.querySelector('.collection');          //The ul
+const clearBtn = document.querySelector(".clear-tasks"); //the all task clear button
 
-const clearBtn = document.querySelector('.clear-tasks');      //the all task clear button
-
-const reloadIcon = document.querySelector('.fa'); //the reload button at the top right of navigation
+const reloadIcon = document.querySelector(".fa"); // the reload button at the top right of navigation
 
 const dropdown = document.querySelector('#dropdown1');
 
 const dropdownSelected = document.querySelector('.dropdown-trigger');
 
-//Add Event Listener [Form, clearBtn and filter search input]
+// sorting buttons
+$(".dropdown-trigger").dropdown();
+const ascendingBtn = document.querySelector(".ascending-btn");
 
-//form submit
-form.addEventListener('submit',addNewTask);
+const descendingBtn = document.querySelector(".descending-btn");
 
-//clear all tasks
-clearBtn.addEventListener('click',clearAllTasks);
+const collectionSorted = document.querySelector(".collection-sorted");
 
-//Filter Task
-filter.addEventListener('keyup',filterTasks);
+// Add Event Listener [Form, clearBt and filter search input]
 
-//Remove task event[event delegation]
-taskList.addEventListener('click',removeTask);
+// form submit
+form.addEventListener("submit", addNewTask);
 
-// Event Listener for reload 
-reloadIcon.addEventListener('click', reloadPage);
+// clear All Tasks
+clearBtn.addEventListener("click", clearAllTasks);
 
-//Event Listener for dropdown
-dropdown.addEventListener('click',changeText)
+// Filter Task
+filter.addEventListener("keyup", filterTasks);
 
-//Add New Task Function definition
+// Remove task event [event delegation]
+taskList.addEventListener("click", removeTask);
+
+// Event Listener for reload
+reloadIcon.addEventListener("click", reloadPage);
+
+//Event Listener for ascending and descending buttons
+ascendingBtn.addEventListener("click", ascendingSort);
+descendingBtn.addEventListener("click", descendingSort);
+
+
+// Add new Task Function definition
 function addNewTask(e) {
-    if (taskInput.value === '') {
-        taskInput.style.borderColor = "red";
-        return; //avoiding else statement
-    }
+  const dateID = Date.now();
 
-    // Create an li element when the user adds a task 
-    const li = document.createElement('li');
-    // Adding a class
-    li.className = 'collection-item';
-    // Create text node and append it 
-    li.appendChild(document.createTextNode(taskInput.value));
-    // Create new element for the link 
-    const link = document.createElement('a');
-    // Add class and the x marker for a 
-    link.className = 'delete-item secondary-content';
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    // Append link to li
-    li.appendChild(link);
-    // Append to ul 
-    taskList.appendChild(li);
+  if (taskInput.value == "") {
+    // alert("Enter New Task"); removed
+    taskInput.style.borderColor = "red";
+    return; // Avoiding else statement
+  }
 
+  // Creat an li element when the user adds a task
+  const li = document.createElement("li");
+  // adding a class
+  li.className = "collection-item";
+  // Create text node and append it
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new element for the link
+  const link = document.createElement("a");
+  // Add class and the x marker for a
+  link.className = "delete-item secondary-content";
+  link.innerHTML = '<i class="fa fa-remove"></i>';
 
-   e.preventDefault();    //disable form submission
+  const dateSpan= document.createElement("span");
+  dateSpan.className = "date";
+  dateSpan.style.display = "none";
+  dateSpan.textContent = dateID;
+  li.appendChild(dateSpan);
+  // Append link to li
+  li.appendChild(link);
+  // Append to ul
+  taskList.appendChild(li);
+  taskInput.value = "";
+
+  // alert("Add New Task ..."); removed
+  e.preventDefault(); // Disable from submission
 }
 
-
-//Clear Task Function definition
+// Clear Task Function definition
 function clearAllTasks() {
-    while (taskList.firstChild) {
-        taskList.removeChild(taskList.firstChild);
-    }
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
 }
 
-//Filter tasks function definition
-
+// Filter tasks function definition
 function filterTasks(e) {
-    let searchInput = filter.value;
-    
-    let tasks = document.querySelectorAll('.collection-item');
+  let searchInput = filter.value;
 
-    tasks.forEach(
-        function(currentValue) {
-            if ((currentValue.textContent).indexOf(searchInput) > -1) {
-                currentValue.style.display = "block";
-            }
-            else {
-                currentValue.style.display = "none";
-            }
-    });  
+  const tasks = document.querySelectorAll(".collection-item");
+  
+  tasks.forEach(
+      function (currentValue) {
+      if ((currentValue.textContent).indexOf(searchInput) > -1) {
+        currentValue.style.display = "block";
+      } 
+      else {
+        currentValue.style.display = "none";
+      }
+    }); 
 }
 
-//Remove Task Function definition
+// Remove Task function definition
 function removeTask(e) {
-    if (e.target.parentElement.classList.contains('delete-item')) {
-        if (confirm('Are You Sure about that?')) {
-            e.target.parentElement.parentElement.remove()
-        }
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are You Sure about that ? ")) {
+      e.target.parentElement.parentElement.remove();
     }
+  }
 }
 
 // Reload Page Function 
 function reloadPage() {
-    //using the reload fun on location object 
-    location.reload();
+  // using the reload fun on location object
+  location.reload();
 }
 
-//Change text function
-function changeText(e) {
-    if (e.target.nodeName == "A")  {
-        dropdownSelected.innerHTML = e.target.innerHTML + ' ' + '<i class="fa fa-caret-down"></i>';
-    }
+
+
+// ascending sorting
+function ascendingSort() {
+  const unorderedList = document.querySelectorAll(".collection-item");
+  var orderingList = new Array();
+  const currentTime = Date.now();
+
+  for (let i = 0; i < unorderedList.length; i++) {
+    listItem = unorderedList[i].querySelector(".date");
+    taskListTime = listItem.textContent;
+    let differenceTime = currentTime - taskListTime;
+    orderingList[i] = [differenceTime, i];
+  }
+
+  orderingList.sort();
+
+  for (let i = 0; i < unorderedList.length; i++) {
+    taskList.appendChild(unorderedList[orderingList[i][1]]);
+  }
 }
+
+// descending sorting
+function descendingSort() {
+  const unorderedList = document.querySelectorAll(".collection-item");
+  var orderingList = new Array();
+  const currentTime = Date.now();
+
+  for (let i = 0; i < unorderedList.length; i++) {
+    listItem = unorderedList[i].querySelector(".date");
+    taskListTime = listItem.textContent;
+    let differenceTime = currentTime - taskListTime;
+    orderingList[i] = [differenceTime, i];
+  }
+
+  orderingList.reverse();
+
+  for (let i = 0; i < unorderedList.length; i++) {
+    taskList.appendChild(unorderedList[orderingList[i][1]]);
+  }
+}
+
+
+
+
